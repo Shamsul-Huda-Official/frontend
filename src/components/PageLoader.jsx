aa6bb1abc1bab560f02e7413f6c260c3ddbb6838
ajmalMundumuzhi <muhajmal73@gmail.com>
2026-03-31 11:10:28 +0530

    refactor: image issue while production stage

--- src/components/PageLoader.jsx
+++ src/components/PageLoader.jsx
@@ -2,7 +2,7 @@ export default function PageLoader() {
     return (
         <div className="flex items-center justify-center h-screen">
             <div className="w-40 h-40 opacity-100 md:w-52 md:h-52 animate-pulse">
-                <img src="./assets/images/shia-logo-p.png" alt="" className="object-contain w-full h-full" />
+                <img src="./images/shia-logo-p.png" alt="" className="object-contain w-full h-full" />
             </div>
         </div>
     )
