d69d5ea7f79975b8a15d552adcfc27d6606fdeb0
ajmalMundumuzhi <muhajmal73@gmail.com>
2026-04-01 18:11:21 +0530

    refactor: Minor changes in hero page code

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,17 +5,14 @@ export default function Hero() {
     return (
         <section className="relative w-[95vw] h-screen overflow-hidden rounded-2xl mx-auto my-10">
 
-            {/* Image */}
             <img 
                 src={college}
                 alt="College"
                 className="object-cover w-full h-full"
             />
 
-            {/* Overlay */}
             <div className="absolute inset-0 bg-gradient-to- t from-white to-white/10" />
 
-            {/* Bottom Left Title (same style as before) */}
             <div className="absolute z-10 bottom-10 left-6">
                 <h1 className="text-5xl font-black leading-none text-white md:text-[clamp(5rem,14vw,6rem)] ">
                     Shamsul Huda Islamic Academy
